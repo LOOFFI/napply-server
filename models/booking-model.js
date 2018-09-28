7cf150ceac18918fb3e5df2fd40c7f77e0ad97b3
Constance Reversé <constance.reverse@hotmail.fr>
2018-09-28 12:46:41 +0200

    err

--- models/booking-model.js
+++ models/booking-model.js
@@ -2,21 +2,19 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const bookingSchema = new Schema ({
-
- 
-  user_id: {type: Schema.Types.ObjectId, required:true},
-  date: {type: Date, required:true},
-  booked: {type: Boolean, default: true},
-  truck_id: {type: Number, required: true},
-  options: {type: [String]}
-  
-},{
-  timestamps:true
-});
+const bookingSchema = new Schema(
+  {
+    user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    date: { type: Date, required: true },
+    booked: { type: Boolean, default: true },
+    truck_id: { type: Number, required: true },
+    options: { type: [String] }
+  },
+  {
+    timestamps: true
+  }
+);
 
 const Booking = mongoose.model("Booking", bookingSchema);
 
 module.exports = Booking;
-
-
--- routes/booking-router.js
+++ routes/booking-router.js
@@ -1,32 +1,33 @@
 const express = require("express");
-const Booking = require("../models/booking-model.js")
-const User = require("../models/user-model.js")
+const Booking = require("../models/booking-model.js");
+const User = require("../models/user-model.js");
 const router = express.Router();
 
-
-router.get("/booking-date", (req,res,next) => {
-  
-  Booking.find({ date: { $gt: new Date(year,month,day,0,0), $lt: new Date(year,month,day,23,59) } })
+router.get("/booking-date", (req, res, next) => {
+  Booking.find({
+    date: {
+      $gt: new Date(year, month, day, 0, 0),
+      $lt: new Date(year, month, day, 23, 59)
+    }
+  })
     .then(bookingResults => res.json(bookingResults))
-    .catch(err => next(err))
-})
+    .catch(err => next(err));
+});
 
-router.post("/booking-date", (res,req,next) => {
-  const {booked,user_id,date} = req.body;
-  Booking.create({booked,user_id,date})
+router.post("/booking-date", (res, req, next) => {
+  const { booked, date } = req.body;
+  const user_id = req.user._id;
+
+  Booking.create({ booked, user_id, date })
     .then(bookingDoc => res.json(bookingDoc))
     .catch(err => next(err));
-})
+});
 
-
-router.delete("/booking-date/:id", (res,req,next) => {
-  const {id} = req.params;
+router.delete("/booking-date/:id", (res, req, next) => {
+  const { id } = req.params;
   Booking.findByIdAndRemove(id)
     .then(bookingDoc => res.json(bookingDoc))
     .catch(err => next(err));
-})
-
-
-
+});
 
 module.exports = router;
