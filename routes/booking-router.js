e30f4040944cf5dea3286258410d31c0af7ad568
Alice Debarge <alicedbrg@gmail.com>
2018-10-03 10:00:01 +0200

    msg

--- routes/booking-router.js
+++ routes/booking-router.js
@@ -14,27 +14,26 @@ router.post("/location", (req, res, next) => {
 router.put("/options/:id", (req, res, next) => {
   const { id } = req.params;
   const { sound, plaid, energyShot } = req.body;
-  
+
   Booking.findByIdAndUpdate(
     id,
     { $set: { sound, plaid, energyShot } },
     { runValidators: true, new: true }
   )
-  .then(bookingDoc => res.json(bookingDoc))
-  .catch(err => next(err));
+    .then(bookingDoc => res.json(bookingDoc))
+    .catch(err => next(err));
 });
 
-
 router.post("/booking-date", (req, res, next) => {
-      console.log( req.body );
-      
-      const { selectedDay } = req.body;
-      const year = selectedDay.slice(0,4);
-      console.log(year);
-      const month = (selectedDay.slice(5,7)-1);
-      console.log(month);
-      const day = selectedDay.slice(8,10);
-      console.log(day);
+  console.log(req.body);
+
+  const { selectedDay } = req.body;
+  const year = selectedDay.slice(0, 4);
+  console.log(year);
+  const month = selectedDay.slice(5, 7) - 1;
+  console.log(month);
+  const day = selectedDay.slice(8, 10);
+  console.log(day);
   Booking.find({
     date: {
       $gt: new Date(year, month, day, 0, 0),
@@ -45,23 +44,22 @@ router.post("/booking-date", (req, res, next) => {
     .catch(err => next(err));
 });
 
-
-
 router.post("/booking-date/:id", (req, res, next) => {
-  const {id} = req.params;
+  const { id } = req.params;
   const { selectedDay, slot } = req.body;
-  const date = selectedDay.slice(0,10)+" "+slot+":00";
-  console.log(req.body)
-  Booking.findByIdAndUpdate(
-    id,
-  {$set: {date}},
-  {runValidators: true}
-  )
+  const date = selectedDay.slice(0, 10) + " " + slot + ":00";
+  console.log(req.body);
+  Booking.findByIdAndUpdate(id, { $set: { date } }, { runValidators: true })
     .then(bookingDoc => res.json(bookingDoc))
     .catch(err => next(err));
 });
 
-
+router.get("/booking/:id", (req, res, next) => {
+  const { id } = req.params;
+  Booking.findById(id)
+    .then(bookingDoc => res.json(bookingDoc))
+    .catch(err => next(err));
+});
 
 // router.delete("/booking-date/:id", (req, res, next) => {
 //   const { id } = req.params;
